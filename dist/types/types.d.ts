89df16dec4bd9f052fbe3814eb4cdce80350e611
Andrey Kuznecov <a.kuznecov@2gis.ru>
2023-02-06 13:38:38 +0700

    add dist

---
+++ dist/types/types.d.ts
@@ -0,0 +1,19 @@
+import { DeckProps } from '@deck.gl/core/typed';
+export interface DeckCustomLayer {
+    type: 'custom';
+    id: string;
+    render: (gl: WebGLRenderingContext) => void;
+    props: any;
+}
+/**
+ * CustomRenderProps is type extends from DeckProps:
+ * https://deck.gl/docs/api-reference/core/deck#properties
+ */
+export type CustomRenderProps = Partial<DeckProps> & CustomRenderInternalProps;
+/**
+ * RenderProps is type extends from DeckProps:
+ * https://deck.gl/docs/api-reference/core/deck#properties
+ */
+export type RenderProps = Partial<DeckProps> & {
+    skipResizeRenderer?: boolean;
+};
