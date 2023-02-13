e230b34e7ac18e92daa9891ccedb478ecc3add0d
Andrey Kuznecov <a.kuznecov@2gis.ru>
2023-02-13 16:25:34 +0700

    add dist to test

---
+++ dist/types/utils.d.ts
@@ -0,0 +1,9 @@
+import type { Map } from '@2gis/mapgl/types';
+import { CustomRenderProps } from './types';
+import { DeckProps } from '@deck.gl/core/typed';
+/**
+ * Initializes deck.gl properties for working with the MapGL map.
+ * @param map The map instance.
+ * @param deckProps CustomRenderProps initialization options.
+ */
+export declare function initDeck2gisProps(map: Map, deckProps?: CustomRenderProps): DeckProps;
