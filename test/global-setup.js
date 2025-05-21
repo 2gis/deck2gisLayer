8c3cfd4ee21992c275e912b53921b5eb6d51d0b0
Dmitry Nishcheta <nish.dima@gmail.com>
2025-05-21 11:01:14 +0500

    Bump jest-dev-server, fix test on updated node

--- test/global-setup.js
+++ test/global-setup.js
@@ -1,7 +1,7 @@
 const { setup: setupDevServer } = require('jest-dev-server');
 
 module.exports = async function globalSetup() {
-    await setupDevServer({
+    global.__JEST_DEV_SERVER_PROCS__ = await setupDevServer({
         command: `http-server -p 8080 ./dist`,
         launchTimeout: 90000,
         port: 8080,
