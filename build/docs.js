e43bd1a554c20fbbeffb0001c7a00c278fce75af
Dmitry Nischeta <55282960+dnischeta@users.noreply.github.com>
2025-05-21 11:03:01 +0500

    Generate docs with @2gis/ts-docs-generator (#41)
    * Generate docs with @2gis/ts-docs-generator
    
    * Try to fix test job
    
    * Migrate to @2gis/ts-docs-generator
    
    * Update workflow
    
    * Bump @2gis/ts-docs-generator
    
    * Remove redundant env
    
    * Allow openssl legacy
    
    * Bump jest-dev-server, fix test on updated node
    
    ---------
    
    Co-authored-by: Dmitry Nishcheta <nish.dima@gmail.com>

---
+++ build/docs.js
@@ -0,0 +1,26 @@
+const { generateDocs } = require('@2gis/ts-docs-generator');
+const fs = require('fs');
+const path = require('path');
+
+const version = process.env.VERSION || 'branch';
+fs.mkdirSync(path.join('dist', 'docs', version), { recursive: true });
+
+generateDocs({
+    version,
+    project: 'deck2gisLayer',
+    defaultReference: 'Deck2gisLayer',
+    docsHost: 'https://unpkg.com/@2gis/deck2gis-layer@^2/dist/docs',
+    excludePaths: [],
+    globs: ['src/**/*'],
+    ignoreMarkdown: true,
+    legacyOutPath: 'dist/docs.json',
+})
+    .then((result) => {
+        fs.writeFileSync(path.join('dist', 'docs', 'manifest.json'), result.manifest);
+        fs.writeFileSync(path.join('dist', 'docs', version, 'en.json'), result.reference.en);
+        fs.writeFileSync(path.join('dist', 'docs', version, 'ru.json'), result.reference.ru);
+    })
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    });
--- test/global-teardown.js
+++ test/global-teardown.js
@@ -1,5 +1,7 @@
 const { teardown: teardownDevServer } = require('jest-dev-server');
 
 module.exports = async function globalTeardown() {
-    await teardownDevServer();
+    if (global.__JEST_DEV_SERVER_PROCS__) {
+        await teardownDevServer(global.__JEST_DEV_SERVER_PROCS__);
+    }
 };
